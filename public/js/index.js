2f864f03362daa59cb5495573263adf51cf9b801
Michael Yiu <onew1ng3d@gmail.com>
2018-10-23 00:01:18 -0400

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    
-
-    socket.emit('createMessage', {
-        from: 'chelsea@example.com',
-        text: 'sup dude'
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,22 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
     
-
-    socket.emit('newMessage', {
-        from: 'mike@example.com',
-        text: 'Hey, whats goin on',
-        createAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('create Message', message);
-        
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     })
 });
+
 server.listen(port, () => {
     console.log(`Started listening on ${port}`);
     
